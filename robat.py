3cc50302d2a628a1f558151948e1fdd940eb244a
ARC19-dev <arefhormoziv19@gmail.com>
2020-09-11 10:42:21 +0430

    Download Youtube

--- robat.py
+++ robat.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, 
                              QPushButton, QLabel, QLineEdit, QVBoxLayout)
@@ -5,6 +6,7 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow,
 from PyQt5.QtGui import QPixmap
 from PyQt5.Qt import Qt
 import time
+import pytube
 import webbrowser
 import speech_recognition as sr
 import pyttsx3
@@ -69,7 +71,7 @@ class Window(QMainWindow):
         self.Titlebox = QLineEdit(self)
         self.Titlebox.resize(170, 25)
         self.Titlebox.move(230, 180)
-        self.Titlebox.setMaxLength(30)
+        self.Titlebox.setMaxLength(50)
         self.Titlebox.returnPressed.connect(self.robat)
 
         self.btn_exit = QPushButton('exit', self)
@@ -93,11 +95,13 @@ class Window(QMainWindow):
         self.btn_capture.clicked.connect(self.capture)        
 
     def voice_robat(self):
-        with sr.Microphone() as source:
+            
             try:
-                recognizer.adjust_for_ambient_noise(source)
-                audio_text = recognizer.listen(source)
-                self.robat(audio_text=recognizer.recognize_google(audio_text))
+                with sr.Microphone() as source:
+                    recognizer.adjust_for_ambient_noise(source)
+                    audio_text = recognizer.listen(source)
+                    text = recognizer.recognize_google(audio_text)
+                    self.robat(audio_text=text)
 
             except:
                 engine.say('Sorry')
@@ -132,22 +136,24 @@ class Window(QMainWindow):
             engine.say('I was before you were born')
             engine.runAndWait()
         
-        elif text.split()[0] in ('google', 'Google', 'bmbgk'):
+        elif text.split()[0] in ('google', 'Google', 'bmbgk'):   # google search
             engine.say('OK')
             engine.runAndWait()
             google_text = ''
             google_search = text.split()[1:]
+            
             for search in google_search:
                 google_text += search
                 google_text += ' '
             webbrowser.open('http://bmbgk.ir/?q={0}'.format(google_text))
 
 
-        elif text.split()[0] in ('youtube', 'Youtube'):
+        elif text.split()[0] in ('youtube', 'Youtube'):   # youtube search 
             engine.say('OK')
             engine.runAndWait()
             youtube_search = text.split()[1:]
             youtube_text = ''
+            
             if youtube_search == []:
                 webbrowser.open('https://www.youtube.com/')
             else:
@@ -155,6 +161,25 @@ class Window(QMainWindow):
                     youtube_text += search
                     youtube_text += ' '
                 webbrowser.open('https://www.youtube.com/results?search_query={0}'.format(youtube_text))
+        
+        elif text.split('/')[2] == 'youtu.be':   # download youtube
+            url = self.Titlebox.text()
+            path = 'C:\\Users\\User\\Desktop\\Download Youtube'
+            engine.say('Download now')
+            engine.runAndWait()
+            
+            if os.path.isdir(path):
+                youtube = pytube.YouTube(url)
+                video = youtube.streams.first()
+                video.download(path)
+            else:
+                os.mkdir(path)
+                youtube = pytube.YouTube(url)
+                video = youtube.streams.first()
+                video.download(path)
+
+            engine.say('Finished download')
+            engine.runAndWait()
 
     def capture(self):
         '''
